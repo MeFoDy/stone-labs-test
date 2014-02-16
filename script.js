b2ced7de3b2ece9afc332d4fb92246c96c1c085c
Nikita Dubko <mefody93@gmail.com>
2014-02-16 04:23:36 +0300

    Create static makeup without js

--- script.js
+++ script.js
@@ -8,3 +8,32 @@ $(document).ready(function() {
 	    parallax(3);
 	});
 });
+
+
+
+/**
+$(document.body).on('appear', 'section h3', function(e, $affected) {
+    // this code is executed for each appeared element
+    $(this).yellowFade();
+
+    $appeared.empty();
+    $affected.each(function() {
+      $appeared.append(this.innerHTML+"\n");
+    })
+  });
+
+  $(document.body).on('disappear', 'section h3', function(e, $affected) {
+    // this code is executed for each disappeared element
+    $disappeared.empty();
+    $affected.each(function() {
+      $disappeared.append(this.innerHTML+"\n");
+    })
+  });
+
+
+  [a-zA-Z][a-zA-Z0-9\.,\-_]{5,31}
+  function validateEmail(email) { 
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+} 
+  */
